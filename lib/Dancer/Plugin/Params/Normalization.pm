128567926454c99c27ac424c4bb97293a53f9c62
Damien Krotkine <dams@cpan.org>
2010-10-13 11:25:45 +0000

    Import of DAMS/Dancer-Plugin-Params-Normalization-0.4 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-Params-Normalization
    gitpan-cpan-version:      0.4
    gitpan-cpan-path:         DAMS/Dancer-Plugin-Params-Normalization-0.4.tar.gz
    gitpan-cpan-author:       DAMS
    gitpan-cpan-maturity:     released

--- lib/Dancer/Plugin/Params/Normalization.pm
+++ lib/Dancer/Plugin/Params/Normalization.pm
@@ -4,7 +4,7 @@ use Dancer ':syntax';
 use Dancer::Plugin;
 
 our $AUTHORITY = 'DAMS';
-our $VERSION = '0.3';
+our $VERSION = '0.4';
 
 my $conf = plugin_setting;
 
@@ -45,8 +45,13 @@ if (defined $conf->{method} && $conf->{method} ne 'passthrough') {
         $method = sub { my ($h) = @_; $apply_on_keys->($h, sub { ucfirst($_[0]) } ) };
     } else {
         my $class = $conf->{method};
-        eval("require $class");
-        $@ and die "error while requiring custom normalization class '$class' : $@";
+        my $class_name = $class;
+        $class_name =~ s!::|'!/!g;
+        $class_name .= '.pm';
+        if ( ! $class->can('new') ) {
+            eval { require $class_name };
+            $@ and die "error while requiring custom normalization class '$class' : $@";
+        }
         my $abstract_classname = __PACKAGE__ . '::Abstract';
         $class->isa(__PACKAGE__ . '::Abstract')
           or die "custom normalization class '$class' doesn't inherit from '$abstract_classname'";
